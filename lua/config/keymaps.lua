c0ca23db6d14c03603df62f6a96f9f5ea3029180
ZZ0075 <ZZ0075@mail.inforce.com.tw>
2025-12-08 16:40:52 +0800

    F / nvim.master / 2025120801 /
         - 一般設定 (lua/config/options.lua)：
           - 已設定 number, expandtab, tabstop=4, clipboard=unnamedplus 等。
           - 已停用 perl 和 ruby provider。
         - 按鍵映射 (lua/config/keymaps.lua)：
           - 已加入系統剪貼簿整合 (Ctrl+C, Ctrl+V)。
           - 已加入 Undo breakpoints (在標點符號後自動建立還原點)。
         - Copilot (lua/plugins/copilot.lua)：
           - 已建立新插件設定檔，安裝 github/copilot.vim。
           - 設定 Ctrl+l 接受建議。
           - 設定 gitcommit 相關的自動啟用與快捷鍵。
         - Treesitter (lua/plugins/treesitter.lua)：
           - 已建立新插件設定檔，確保安裝 lua, javascript, vim, gitcommit, json, bash 等語法分析器。

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- System clipboard integration
+map("v", "<C-c>", '"+y', { desc = "Copy to system clipboard" })
+map("n", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+map("i", "<C-v>", '<C-r>+', { desc = "Paste from system clipboard" })
+map("v", "<C-v>", '"+p', { desc = "Paste from system clipboard" })
+
+-- Undo breakpoints
+local undo_ch = { " ", ",", ".", "!", "?", ";", ":" }
+for _, ch in ipairs(undo_ch) do
+  map("i", ch, ch .. "<C-g>u")
+end
+map("i", "<CR>", "<CR><C-g>u")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.foldenable = false
+vim.opt.foldmethod = "manual"
