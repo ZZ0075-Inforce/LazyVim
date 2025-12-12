8ae669cd4b58c96fc4a66e67f2d423cac2e61d80
ZZ0075 <ZZ0075@mail.inforce.com.tw>
2025-12-12 11:23:32 +0800

    F / nvim.master / 2025121201 /
         ● feat(markdown)：用 cmd /d 開啟 Markdown 預覽，避免 clink AutoRun 多開終端機
         ● ：新增 GitHub Copilot 使用說明文件

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -6,4 +6,25 @@ return {
       { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "Toggle Render Markdown" },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
+      vim.cmd([[
+function! OpenMarkdownPreview(url) abort
+  " /d disables AutoRun (prevents clink/chcp from running)
+  call jobstart(['cmd.exe', '/d', '/c', 'start', '""', a:url], { 'detach': v:true })
+endfunction
+      ]])
+    end,
+    keys = {
+      { "<leader>uP", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
+    },
+  },
 }
