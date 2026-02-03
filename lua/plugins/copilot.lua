105557eb07c7a942bf8bdb5c6dce0388fab92fd9
ZZ0075 <ZZ0075@mail.inforce.com.tw>
2026-02-03 11:10:49 +0800

    F / nvim.main / 2026020302 /
        更換copilot vim官方版本至社群版本
        https://github.com/zbirenbaum/copilot.lua
    
        提高lazyVim的相容性

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,27 +1,25 @@
 return {
   {
-    "github/copilot.vim",
-    config = function()
-      -- Copilot filetypes
-      vim.g.copilot_filetypes = {
-        ["*"] = true,
-        ["gitcommit"] = true,
-        ["markdown"] = true,
-        ["yaml"] = true,
-      }
-
-      -- Keymaps
-      vim.g.copilot_no_tab_map = true
-      vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
-
-      -- Autocmds for gitcommit
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "gitcommit",
-        callback = function()
-          vim.cmd("Copilot enable")
-          vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { buffer = true })
-        end,
-      })
-    end,
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>", -- 使用 Ctrl+l 接受建議
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
   },
 }
